f45ac42a621ea1015dd2be835bdc49407715ed4a
marpusik <marpusik1987@gmail.com>
2021-05-12 16:53:34 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Menu from "./components/menu/Menu";
+import Secondmenu from "./components/secondmenu/Secondmenu";
+import Contract from "./components/contract/Сontract";
+import Social from "./components/social/Social";
+import Email from "./components/email/Email";
 
 function App() {
-  return (
-    <div className="App">
-      Hello!
-    </div>
-  );
+    return (
+        <div className="App">
+            <Menu/>
+            <Secondmenu/>
+            <Contract/>
+            <Social/>
+            <Email/>
+        </div>
+    );
 }
 
 export default App;
